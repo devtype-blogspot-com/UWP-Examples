71d9a0fee0d168cf0141e66f40016074a380a186
DEVTYPE <Informatik.on.GitHub@gmail.com>
2016-08-21 22:43:06 +0600

    Namespaces in WinJS

---
+++ NamespaceDefine/NamespaceDefine/js/WinJSFundamentals.js
@@ -0,0 +1,33 @@
+﻿// Define the Namespace Developerpublish
+WinJS.Namespace.define("DeveloperPublish");
+// Utilities created in the DeveloperPublish Namespace
+DeveloperPublish.Utilities = {
+    DisplayMessage: function () {
+        return "Message from DeveloperPublish Namespace";
+    }
+};
+
+// Define the Namespace Apress and create the Utilities under it.
+WinJS.Namespace.define("Apress",
+ {
+     Utilities: {
+         DisplayMessage: function () {
+             return "Message from Apress Namespace";
+         }
+     }
+ });
+
+console.log(DeveloperPublish.Utilities.DisplayMessage());
+console.log(Apress.Utilities.DisplayMessage());
+
+WinJS.Namespace.defineWithParent(Apress, "Books",
+ {
+     Utilities:
+     {
+         DisplayMessage: function () {
+             return "Message from Apress.Books Namespace";
+         }
+     }
+ }
+);
+console.log(Apress.Books.Utilities.DisplayMessage());
--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
   - [Пример простого UWP-приложения][0]
       - [HelloWorldAppWithUWP][1]
+  - [Пространства имён в WinJS][2]
+      - [NamespaceDefine][3]
 
    [0]: <http://devtype.blogspot.com/2016/08/Primer-prostogo-UWP-prilozhenija.html>
    [1]: <HelloWorldAppWithUWP>
+   [2]: <http://devtype.blogspot.com/2016/08/Prostranstva-imjon-v-WinJS.html>
+   [3]: <NamespaceDefine>
